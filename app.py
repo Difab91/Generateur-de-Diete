19fcdf3598d49662f707c312b7094b3e6c64c222
alexis <alexmovie@hotmail.fr>
2024-05-21 13:36:50 +0200

    amelioration de l'affichage

--- app.py
+++ app.py
@@ -1,98 +1,59 @@
 import streamlit as st
 from gen import algo
-
-def affichage2600(protpt, glupt, lippt, pt, protcol2, glucol2, lipcol2, col2, protdej, gludej, lipdej, dej, protcol1, glucol1, lipcol1, col1, protdin, gludin, lipdin, din, protdiet, gludiet, lipdiet, cal2):
-
-    st.markdown(
-        f'''
-        <p style="font-size: 20px; font-weight: bold;">
-            Petit déjeuner 
-            <span style="font-size: 16px; font-weight: normal;">
-                ({protpt}g de protéines, {glupt}g de glucides, {lippt}g de lipides) :
-            </span>
-        </p>
-        ''',
-        unsafe_allow_html=True
-    )
-
-    st.write(f"{pt} ")
-    st.write(" ")
-    st.markdown(
-        f'''
-            <p style="font-size: 20px; font-weight: bold;">
-                Collation du matin  
-                <span style="font-size: 16px; font-weight: normal;">
-                    ({protcol2}g de protéines, {glucol2}g de glucides, {lipcol2}g de lipides) :
-                </span>
-            </p>
-            ''',
-        unsafe_allow_html=True
-    )
-    st.write(f"{col2} ")
-    st.write(" ")
-    st.markdown(
-        f'''
-            <p style="font-size: 20px; font-weight: bold;">
-                 Dejeuner 
-                <span style="font-size: 16px; font-weight: normal;">
-                    ({protdej}g de protéines, {gludej}g de glucides, {lipdej}g de lipides) :
-                </span>
-            </p>
-            ''',
-        unsafe_allow_html=True
-    )
-    st.write(f" {dej} ")
-    st.write(" ")
-    st.markdown(
-        f'''
-                <p style="font-size: 20px; font-weight: bold;">
-                     Collation de l'aprés midi
-                    <span style="font-size: 16px; font-weight: normal;">
-                        ({protcol1}g de protéines, {glucol1}g de glucides, {lipcol1}g de lipides) :
-                    </span>
-                </p>
-                ''',
-        unsafe_allow_html=True
-    )
-    st.write(f" {col1} ")
-    st.write(" ")
-    st.markdown(
-        f'''
-                    <p style="font-size: 20px; font-weight: bold;">
-                         Dinner
-                        <span style="font-size: 16px; font-weight: normal;">
-                            ({protdin}g de protéines, {gludin}g de glucides, {lipdin}g de lipides) :
-                        </span>
-                    </p>
-                    ''',
-        unsafe_allow_html=True
-    )
-    st.write(f" {din} ")
-    st.write(" ")
-    st.markdown(
-        f'''
-        <p style="font-size: 19px; color: blue;">
-            Soit un total sur la journée de {protdiet}g de protéines, {gludiet}g de glucides, {lipdiet}g de lipides ce qui fait {cal2} calories.
-        </p>
-        ''',
-        unsafe_allow_html=True
-    )
-def affichage(protpt, glupt, lippt, pt, protdej, gludej, lipdej, dej, protcol1, glucol1, lipcol1, col1, protdin, gludin, lipdin, din, protdiet, gludiet, lipdiet, cal2):
-    st.write(f" Petit dejeuner: {pt}  ({protpt}g de protéines, {glupt}g de glucides, {lippt}g de lipides)")
-    st.write(f" collation: {col1}  ({protcol1}g de protéines, {glucol1}g de glucides, {lipcol1}g de lipides)")
-    st.write(f" déjeuner: {dej}  ({protdej}g de protéines, {gludej}g de glucides, {lipdej}g de lipides)")
-    st.write(f" Dinner: {din}  ({protdin}g de protéines, {gludin}g de glucides, {lipdin}g de lipides)")
-    st.write(f"Soit un total sur la journée de  {protdiet}g de protéines, {gludiet}g de glucides, {lipdiet}g de lipides ce qui fait {cal2} calories")
+from affichage import affichage
+from affichage import affichage2600
+from affichage import side
 
 
 
 st.header("veuillez rentrer les détail des macronutriments souhaitez :",divider='rainbow')
 
-prot = st.text_input(label="inserer le nombre de protéines", key="user_input1")
-glu = st.text_input(label="inserer le nombre de glucide", key="user_input2")
-lip = st.text_input(label="inserer le nombre de lipide", key="user_input3")
+if 'prot' not in st.session_state:
+    st.session_state.prot = ""
+if 'glu' not in st.session_state:
+    st.session_state.glu = ""
+if 'lip' not in st.session_state:
+    st.session_state.lip = ""
+
+
+
+
+# Demande de saisie des protéines
+prot_placeholder = st.empty()
+prot = prot_placeholder.text_input(label="Insérer le nombre de protéines", key="user_input1",
+                                   value=st.session_state.prot)
+
+if prot:
+    st.session_state.prot = prot
+    prot_placeholder.empty()
+    glu_placeholder = st.empty()
+    glu = glu_placeholder.text_input(label="Insérer le nombre de glucides", key="user_input2",
+                                     value=st.session_state.glu)
+
+    if glu:
+        st.session_state.glu = glu
+        glu_placeholder.empty()
+        lip_placeholder = st.empty()
+        lip = lip_placeholder.text_input(label="Insérer le nombre de lipides", key="user_input3",
+                                         value=st.session_state.lip)
+
+        if lip:
+            st.session_state.lip = lip
+            lip_placeholder.empty()
+
 
 if prot and glu and lip :
+    proti = int(prot)
+    glui = int(glu)
+    lipi = int(lip)
+    obj = 9 * lipi + 4 * (proti + glui)
+    with st.sidebar:
+        side(prot,glu,lip,obj)
+
+
+
+
+
     protacons=int(prot)
     gluacons=int(glu)
     lipacons=int(lip)
@@ -109,8 +70,6 @@ if prot and glu and lip :
     else:
         protpt, glupt, lippt, pt, protdej, gludej, lipdej, dej, protcol1, glucol1, lipcol1, col1, protdin, gludin, lipdin, din, protdiet, gludiet, lipdiet, cal2 = algo(bp, hp, hl, bl, hg, bg, cal)
         affichage(protpt, glupt, lippt, pt, protdej, gludej, lipdej, dej, protcol1, glucol1, lipcol1, col1, protdin, gludin, lipdin, din, protdiet, gludiet, lipdiet, cal2)
-
-
 
 
 
